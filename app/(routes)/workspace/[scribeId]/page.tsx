e97d6e82be0755b90cba581a77bf6019b61e2477
Caleb Ganza <icaleb130@gmail.com>
2025-01-10 17:29:18 +0200

    Enabling async functionalities

--- app/(routes)/workspace/[scribeId]/page.tsx
+++ app/(routes)/workspace/[scribeId]/page.tsx
@@ -6,36 +6,42 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 const ScribeId = async ({ params }: { params: { scribeId: string } }) => {
-  // Await params before accessing its properties
-  const { scribeId } = params;
-
+  // Await params before accessing its propertie
+  const { scribeId } = await params;
   if (!scribeId) {
     return redirect("/");
   }
 
-  // Fetch the scribe from the database
-  const scribe = await db
-    .select()
-    .from(scribes)
-    .where(eq(scribes.id, scribeId))
-    .limit(1);
+  let scribe = null;
+  try {
+    scribe = await db
+      .select()
+      .from(scribes)
+      .where(eq(scribes.id, scribeId))
+      .limit(1);
 
-  if (scribe.length < 1) {
+    if (scribe.length < 1) {
+      return redirect("/");
+    }
+
+    const { html, css, js } = scribe[0];
+
+    return (
+      <div>
+        <CodeEditor
+          initialHtml={html}
+          initialCss={css}
+          initialJs={js}
+          id={scribeId}
+        />
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching scribe:", error);
+
+    // Handle redirect if there's an error
     return redirect("/");
   }
-
-  const { html, css, js } = scribe[0];
-
-  return (
-    <div>
-      <CodeEditor
-        initialHtml={html}
-        initialCss={css}
-        initialJs={js}
-        id={scribeId}
-      />
-    </div>
-  );
 };
 
 export default ScribeId;
