3072c483af973c1fa04151901bcdd2059d0fe0ca
Caleb Ganza <icaleb130@gmail.com>
2025-01-17 20:04:21 +0200

    the debugging of the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 "use client";
-import DisplayCards from "@/components/Features";
-import Features from "@/components/Features";
-import Header from "@/components/Header";
+
 import Preview from "@/components/Hero";
-import Hero from "@/components/Hero";
 
 import Navigation from "@/components/Navigation";
-import { DisplayCardsDemo } from "@/components/Section";
 
 export default function Home() {
   return (
