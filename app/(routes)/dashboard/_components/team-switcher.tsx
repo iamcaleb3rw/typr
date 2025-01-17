6c77e3282161cda24d8a696cfbf4f39f208c018c
Caleb Ganza <icaleb130@gmail.com>
2025-01-17 23:58:19 +0200

    Improvinh accessibility

--- app/(routes)/dashboard/_components/team-switcher.tsx
+++ app/(routes)/dashboard/_components/team-switcher.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react";
 import { WiMeteor } from "react-icons/wi";
+import Link from "next/link";
 
 import {
   SidebarMenu,
@@ -18,19 +19,21 @@ export function TeamSwitcher() {
 
   return (
     <SidebarMenu>
-      <SidebarMenuItem>
-        <SidebarMenuButton
-          size="lg"
-          className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-        >
-          <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-            <WiMeteor size={20} />
-          </div>
-          <div className="grid flex-1 text-left text-sm leading-tight">
-            <span className="truncate-1 font-semibold">typr</span>
-          </div>
-        </SidebarMenuButton>
-      </SidebarMenuItem>
+      <Link href={"/"}>
+        <SidebarMenuItem>
+          <SidebarMenuButton
+            size="lg"
+            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+          >
+            <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+              <WiMeteor size={20} />
+            </div>
+            <div className="grid flex-1 text-left text-sm leading-tight">
+              <span className="truncate-1 font-semibold">typr</span>
+            </div>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      </Link>
     </SidebarMenu>
   );
 }
