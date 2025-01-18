6a4bbb5d3b2ebfd808f106f724b74d3eaeb850a9
Caleb Ganza <icaleb130@gmail.com>
2025-01-18 13:49:45 +0200

    Metadata optimization

---
+++ app/(routes)/dashboard/leaderboard/loading.tsx
@@ -0,0 +1,8 @@
+import { Skeleton } from "@heroui/skeleton";
+export default function Loading() {
+  return (
+    <div className="w-full border">
+      <Skeleton />
+    </div>
+  );
+}
--- app/(routes)/dashboard/leaderboard/page.tsx
+++ app/(routes)/dashboard/leaderboard/page.tsx
@@ -2,34 +2,12 @@ import { db } from "@/db";
 import { coders, scribes, likes } from "@/db/schema";
 import { sql, eq, desc } from "drizzle-orm";
 import React from "react";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { Heart } from "lucide-react";
 import LeaderboardTable from "@/components/LeaderboardTable";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 const page = async () => {
-  // Fetch coders whose scribes received the most likes
-  const coderWithMostLikes = await db
-    .select({
-      coderId: coders.id,
-      username: coders.username,
-      email: coders.email,
-      totalLikes: sql<number>`COUNT(${likes.id})`.as("totalLikes"),
-    })
-    .from(coders)
-    .leftJoin(scribes, eq(coders.id, scribes.authorId)) // Join coders with their scribes
-    .leftJoin(likes, eq(scribes.id, likes.scribeId)) // Join scribes with likes
-    .groupBy(coders.id, coders.username, coders.email)
-    .orderBy(desc(sql<number>`COUNT(${likes.id})`));
-
-  console.log(coderWithMostLikes); // Order by the number of likes // Fetch top 10 coders
+  // Fetch coders whose scribes received the most likes // Order by the number of likes // Fetch top 10 coders
 
   return (
     <div className="px-3">
@@ -37,7 +15,9 @@ const page = async () => {
         Leaderboard
       </h1>
       <div className="mt-4  overflow-auto">
-        <LeaderboardTable />
+        <Suspense fallback={<Loading />}>
+          <LeaderboardTable />
+        </Suspense>
       </div>
     </div>
   );
